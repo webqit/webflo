d1b26bf056d729606a44421c517bccd3d7bb1385
Oxford Harrison <oxharris.me@gmail.com>
2021-03-29 07:09:42 +0100

    Clean

--- modules/server/Server.js
+++ modules/server/Server.js
@@ -23,15 +23,6 @@ import Router, { FixedResponse } from './Router.js';
 import * as config from '../../config/index.js';
 import * as cmd from '../../cmd/index.js';
 
-import * as _layout from '../../config/layout.js';
-import * as _variables from '../../config/variables.js';
-import * as server from '../../config/server.js';
-import * as vhosts from '../../config/vhosts.js';
-import * as headers from '../../config/headers.js';
-import * as redirects from '../../config/redirects.js';
-import * as prerendering from '../../config/prerendering.js';
-import * as origins from '../../cmd/origins.js';
-
 /**
  * The default initializer.
  * 
@@ -41,55 +32,61 @@ import * as origins from '../../cmd/origins.js';
  * @return void
  */
 export default async function(Ui, flags = {}) {
-    const layout = await config.layout.read(flags, {});
-    const server = await config.server.read(flags, layout);
-    const variables = await config.variables.read(flags, {});
 
-    if (variables.autoload) {
-        Object.keys(variables.entries).forEach(key => {
-            process.env[key] = variables.entries[key];
+    const setup = {
+        layout: await config.layout.read(flags, {}),
+        server: await config.server.read(flags, layout),
+        variables: await config.variables.read(flags, {}),
+    };
+
+    if (setup.variables.autoload) {
+        Object.keys(setup.variables.entries).forEach(key => {
+            process.env[key] = setup.variables.entries[key];
         });
     }
 
-    const instanceConfig = config;
+    const instanceSetup = setup;
     
-    const v_configs = {};
-    if (server.shared) {
-        await Promise.all(((await config.vhosts.read(flags, layout)).entries || []).map(async vh => {
-            const vlayout = await config.layout.read(flags, {ROOT: Path.join(layout.ROOT, vh.path)});
-            const vserver = await config.server.read(flags, vlayout);
-            const vvariables = await config.variables.read(flags, vlayout);
-            v_configs[vh.host] = { layout: vlayout, config: vconfig, vh, };
+    const v_setup = {};
+    if (setup.server.shared) {
+        await Promise.all(((await config.vhosts.read(flags, setup.layout)).entries || []).map(async vh => {
+            const vlayout = await config.layout.read(flags, {ROOT: Path.join(setup.layout.ROOT, vh.path)});
+            v_setup[vh.host] = {
+                layout: vlayout,
+                server: await config.server.read(flags, vlayout),
+                variables: await config.variables.read(flags, vlayout),
+                vh,
+            };
         }));
     }
 
     // ---------------------------------------------
     
-    const getVConfig = (request, response) => {
-        var v_config, v_hostname = (request.headers.host || '').split(':')[0];
-        if (v_config = v_configs[v_hostname]) {
-            return v_config;
+    const getVSetup = (request, response) => {
+        var _setup, v_hostname = (request.headers.host || '').split(':')[0];
+        if (_setup = v_setup[v_hostname]) {
+            return _setup;
         }
-        if ((v_hostname.startsWith('www.') && (v_config = v_configs[v_hostname.substr(4)]) && v_config.config.server.force_www)
-        || (!v_hostname.startsWith('www.') && (v_config = v_configs['www.' + v_hostname]) && v_config.config.server.force_www)) {
-            return v_config;
+        if ((v_hostname.startsWith('www.') && (_setup = v_setup[v_hostname.substr(4)]) && _setup.server.force_www)
+        || (!v_hostname.startsWith('www.') && (_setup = v_setup['www.' + v_hostname]) && _setup.server.force_www)) {
+            return _setup;
         }
         response.statusCode = 500;
         response.end('Unrecognized host');
     };
 
-    const goOrForceWww = (layout, config, request, response, protocol, isVhost) => {
+    const goOrForceWww = (setup, request, response, protocol, isVhost) => {
         var hostname = request.headers.host || '';
-        if (hostname.startsWith('www.') && config.server.force_www === 'remove') {
+        if (hostname.startsWith('www.') && setup.server.force_www === 'remove') {
             response.statusCode = 302;
             response.setHeader('Location', protocol + '://' + hostname.substr(4) + request.url);
             response.end();
-        } else if (!hostname.startsWith('www.') && config.server.force_www === 'add') {
+        } else if (!hostname.startsWith('www.') && setup.server.force_www === 'add') {
             response.statusCode = 302;
             response.setHeader('Location', protocol + '://www.' + hostname + request.url);
             response.end();
         } else {
-            run(instanceConfig, layout, config, request, response, Ui, flags, protocol, isVhost);
+            run(instanceSetup, setup, request, response, Ui, flags, protocol);
         }
     };
 
@@ -98,23 +95,23 @@ export default async function(Ui, flags = {}) {
     if (!flags['https-only']) {
 
         Http.createServer((request, response) => {
-            if (config.server.shared) {
-                var v_config;
-                if (v_config = getVConfig(request, response)) {
-                    goOrForceHttps(v_config.layout, v_config.config, request, response, v_config.vh);
+            if (setup.server.shared) {
+                var _setup;
+                if (_setup = getVSetup(request, response)) {
+                    goOrForceHttps(_setup, request, response);
                 }
             } else {
-                goOrForceHttps(layout, config, request, response);
+                goOrForceHttps(setup, request, response);
             }
-        }).listen(config.server.port);
+        }).listen(setup.server.port);
 
-        const goOrForceHttps = (_layout, _config, _request, _response, isVhost) => {
-            if (_config.server.https.force && !flags['http-only'] && /** main server */config.server.https.port) {
-                _response.statusCode = 302;
-                _response.setHeader('Location', 'https://' + _request.headers.host + _request.url);
-                _response.end();
+        const goOrForceHttps = ($setup, $request, $response) => {
+            if ($setup.server.https.force && !flags['http-only'] && /** main server */setup.server.https.port) {
+                $response.statusCode = 302;
+                $response.setHeader('Location', 'https://' + $request.headers.host + $request.url);
+                $response.end();
             } else {
-                goOrForceWww(_layout, _config, _request, _response, 'http', isVhost);
+                goOrForceWww($setup, $request, $response, 'http');
             }
         };
 
@@ -122,51 +119,50 @@ export default async function(Ui, flags = {}) {
 
     // ---------------------------------------------
 
-    if (!flags['http-only'] && config.server.https.port) {
+    if (!flags['http-only'] && setup.server.https.port) {
 
         const httpsServer = Https.createServer({}, (request, response) => {
-            if (config.server.shared) {
-                var v_config;
-                if (v_config = getVConfig(request, response)) {
-                    goOrForceWww(v_config.layout, v_config.config, request, response, 'https', v_config.vh);
+            if (setup.server.shared) {
+                var _setup;
+                if (_setup = getVSetup(request, response)) {
+                    goOrForceWww(_setup, request, response, 'https');
                 }
             } else {
-                goOrForceWww(layout, config, request, response, 'https');
+                goOrForceWww(setup, request, response, 'https');
             }
         });
 
-        if (config.server.shared) {
-            _each(v_configs, (host, v_config) => {
-                if (Fs.existsSync(v_config.config.server.https.keyfile)) {
+        if (setup.server.shared) {
+            _each(v_setup, (host, _setup) => {
+                if (Fs.existsSync(_setup.server.https.keyfile)) {
                     const cert = {
-                        key: Fs.readFileSync(v_config.config.server.https.keyfile),
-                        cert: Fs.readFileSync(v_config.config.server.https.certfile),
+                        key: Fs.readFileSync(_setup.server.https.keyfile),
+                        cert: Fs.readFileSync(_setup.server.https.certfile),
                     };
                     httpsServer.addContext(host, cert);
-                    if (v_config.config.server.force_www) {
+                    if (_setup.server.force_www) {
                         httpsServer.addContext(host.startsWith('www.') ? host.substr(4) : 'www.' + host, cert);
                     }
                 }
             });
         } else {
-            if (Fs.existsSync(config.server.https.keyfile)) {
+            if (Fs.existsSync(setup.server.https.keyfile)) {
                 httpsServer.addContext('*', {
-                    key: Fs.readFileSync(config.server.https.keyfile),
-                    cert: Fs.readFileSync(config.server.https.certfile),
+                    key: Fs.readFileSync(setup.server.https.keyfile),
+                    cert: Fs.readFileSync(setup.server.https.certfile),
                 });
             }
         }
 
-        httpsServer.listen(config.server.https.port);
+        httpsServer.listen(setup.server.https.port);
     }
 };
 
 /**
  * The Server.
  * 
- * @param Object    instanceConfig
- * @param Object    layout
- * @param Object    config
+ * @param Object    instanceSetup
+ * @param Object    hostSetup
  * @param Request   request
  * @param Response  response
  * @param Ui        Ui
@@ -175,13 +171,12 @@ export default async function(Ui, flags = {}) {
  * 
  * @return void
  */
-export async function run(instanceConfig, layout, config, request, response, Ui, flags = {}, protocol = 'http', vhost = null) {
+export async function run(instanceSetup, hostSetup, request, response, Ui, flags = {}, protocol = 'http') {
 
-    const $layout = {...layout};
     request.URL = Url.parse(protocol + '://' + request.headers.host + request.url, true, true);
     const $context = {
         rdr: null,
-        layout: $layout,
+        layout: hostSetup.layout,
         env: {},
         // Piping utility
         walk: async (...stack) => {
@@ -200,9 +195,9 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
         fatal: false,
     };
 
-    if (config.variables.autoload) {
-        Object.keys(config.variables.entries).forEach(key => {
-            $context.env[key] = config.variables.entries[key];
+    if (hostSetup.variables.autoload) {
+        Object.keys(hostSetup.variables.entries).forEach(key => {
+            $context.env[key] = hostSetup.variables.entries[key];
         });
     }
 
@@ -211,7 +206,7 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
     // -------------------
 
     if (config.redirects) {
-        if ($context.rdr = await config.redirects.match(request.URL, flags, $layout)) {
+        if ($context.rdr = await config.redirects.match(request.URL, flags, hostSetup.layout)) {
             response.statusCode = $context.rdr.code;
             response.setHeader('Location', $context.rdr.target);
             response.end();
@@ -304,15 +299,15 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
             // Handle autodeploy events
             // -------------------
 
-            if (config.origins) {
-                config.origins.hook(Ui, request, response, flags, $layout).then(async () => {
-                    await serverRestart(Ui, instanceConfig.server.process.name);
+            if (cmd.origins) {
+                cmd.origins.hook(Ui, request, response, flags, hostSetup.layout).then(async () => {
+                    await serverRestart(Ui, instanceSetup.server.process.name);
                     process.exit();
                 }).catch(e => { throw e; });
             }
 
             // The app router
-            const router = new Router(request.URL.pathname, $layout, $context);
+            const router = new Router(request.URL.pathname, hostSetup.layout, $context);
 
             // --------
             // ROUTE FOR DATA
@@ -327,7 +322,7 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
                 // PRE-RENDERING
                 // ----------------
                 if (file && file.contentType === 'text/html' && (file.content + '').startsWith(`<!-- PRE-RENDERED -->`)) {
-                    var prerenderMatch = config.prerendering ? await !config.prerendering.match(request.URL.pathname, flags, $layout) : null;
+                    var prerenderMatch = config.prerendering ? await !config.prerendering.match(request.URL.pathname, flags, hostSetup.layout) : null;
                     if (!prerenderMatch) {
                         router.deletePreRendered(file.filename);
                         return;
@@ -345,20 +340,20 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
                     // --------
                     const rendering = await router.route('render', [request], $context.response, async function(data) {
                         // --------
-                        if (!$layout.renderFileCache) {
-                            $layout.renderFileCache = {};
+                        if (!hostSetup.layout.renderFileCache) {
+                            hostSetup.layout.renderFileCache = {};
                         }
                         var renderFile, pathnameSplit = request.URL.pathname.split('/');
-                        while ((renderFile = Path.join($layout.ROOT, $layout.PUBLIC_DIR, './' + pathnameSplit.join('/'), 'index.html')) 
-                        && pathnameSplit.length && ($layout.renderFileCache[renderFile] === false || !($layout.renderFileCache[renderFile] && Fs.existsSync(renderFile)))) {
-                            $layout.renderFileCache[renderFile] === false;
+                        while ((renderFile = Path.join(hostSetup.layout.ROOT, hostSetup.layout.PUBLIC_DIR, './' + pathnameSplit.join('/'), 'index.html')) 
+                        && pathnameSplit.length && (hostSetup.layout.renderFileCache[renderFile] === false || !(hostSetup.layout.renderFileCache[renderFile] && Fs.existsSync(renderFile)))) {
+                            hostSetup.layout.renderFileCache[renderFile] === false;
                             pathnameSplit.pop();
                         }
-                        $layout.renderFileCache[renderFile] === true;
+                        hostSetup.layout.renderFileCache[renderFile] === true;
                         const instanceParams = QueryString.stringify({
                             SOURCE: renderFile,
                             URL: request.URL.href,
-                            ROOT: $layout.ROOT,
+                            ROOT: hostSetup.layout.ROOT,
                         });
                         const { window } = await import('@webqit/pseudo-browser/instance.js?' + instanceParams);
                         // --------
@@ -414,7 +409,7 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
                     // Handle headers
                     // -------------------
                     if (config.headers) {
-                        if ($context.headers = await config.headers.match(request.URL, flags, $layout)) {
+                        if ($context.headers = await config.headers.match(request.URL, flags, hostSetup.layout)) {
                             $context.headers.forEach(header => {
                                 response.setHeader(header.name, header.value);
                             });
@@ -436,7 +431,7 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
                     // PRE-RENDERING
                     // ----------------
                     if (!$context.response.filename && $context.response.contentType === 'text/html') {
-                        var prerenderMatch = config.prerendering ? await !config.prerendering.match(request.URL.pathname, flags, $layout) : null;
+                        var prerenderMatch = config.prerendering ? await !config.prerendering.match(request.URL.pathname, flags, hostSetup.layout) : null;
                         if (prerenderMatch) {
                             router.putPreRendered(request.URL.pathname, `<!-- PRE-RENDERED -->\r\n` + $context.response.content);
                         }
@@ -463,7 +458,7 @@ export async function run(instanceConfig, layout, config, request, response, Ui,
 
     if (flags.logs !== false) {
         Ui.log(''
-            + '[' + (vhost ? Ui.style.keyword(vhost.host) + '][' : '') + Ui.style.comment((new Date).toUTCString()) + '] '
+            + '[' + (hostSetup.vh ? Ui.style.keyword(hostSetup.vh.host) + '][' : '') + Ui.style.comment((new Date).toUTCString()) + '] '
             + Ui.style.keyword(protocol.toUpperCase() + ' ' + request.method) + ' '
             + Ui.style.url(request.url) + ($context.response && $context.response.autoIndex ? Ui.style.comment((!request.url.endsWith('/') ? '/' : '') + $context.response.autoIndex) : '') + ' '
             + ($context.response ? ' (' + Ui.style.comment($context.response.contentType) + ') ' : '')
