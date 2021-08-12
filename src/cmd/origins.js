ac4a535ab4e49591fbcbb2ba693bc68b2ba598e4
Oxford Harrison <oxharris.me@gmail.com>
2021-08-12 18:05:19 +0100

    Fixed bugs with autodeploy autorun

--- src/cmd/origins.js
+++ src/cmd/origins.js
@@ -90,9 +90,16 @@ export async function deploy(Ui, origin, flags = {}, layout = {}) {
                         cmd = cmd.split(' ').map(a => a.trim()).filter(a => a);
                         const child = spawn(cmd.shift(), cmd, {
                             cwd: origin.deploy_path,
-                            //stdio: 'pipe',
+                            stdio: 'pipe',
                         });
-                        process.stdin.pipe(child.stdin);
+
+                        child.stdout.on('data', data => {
+                            Ui.log('[' + Ui.style.keyword('ONDEPLOY') + '][' + Ui.style.var('OUT') + ']:', data + '');
+                        });
+
+                        child.stderr.on('data', data => {
+                            Ui.log('[' + Ui.style.keyword('ONDEPLOY') + '][' + Ui.style.err('ERR') + ']:', (data + '').trim());
+                        });
                         
                         child.on('error', data => {
                             Ui.error(data);
