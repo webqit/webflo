d9306b2426158e4ae0ddba982ab026fb95af1db1
Oxford Harrison <oxharris.dev@gmail.com>
2022-07-25 02:44:07 +0100

    Edit README

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@
 import { config, runtime, Context } from '../src/index.js';
 
 let client = {
-    handle: function(httpEvent) {
+    handle: function( httpEvent ) {
         return new httpEvent.Response({ abcd: '1234' }, {
             status: 302,
             headers: {
@@ -18,8 +18,8 @@ let client = {
     },
 };
 
-const cx = Context.create({ config: config, });
-const clientCallback = (_cx, hostName, defaultClientCallback) => client;
-const app = await runtime.server.start.call(cx, clientCallback);
+const cx = Context.create( { config: config, } );
+const clientCallback = ( _cx, hostName, defaultClientCallback ) => client;
+const app = await runtime.server.start.call( cx, clientCallback );
 
-const response = await app.go('http://localhost/', { headers: { range: 'bytes=0-5, 6' } } );
+const response = await app.go( 'http://localhost/', { headers: { range: 'bytes=0-5, 6' } } );
