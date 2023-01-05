717d669862d0968fcfebb44884cd80a783cb5b11
Oxford Harrison <oxharris.dev@gmail.com>
2023-01-05 18:15:16 +0100

    fix: service worker always returning json response

--- src/runtime-pi/client/worker/Runtime.js
+++ src/runtime-pi/client/worker/Runtime.js
@@ -86,18 +86,19 @@ export default class Runtime extends _Runtime {
 		self.addEventListener('fetch', event => {
 			// URL schemes that might arrive here but not supported; e.g.: chrome-extension://
 			if (!event.request.url.startsWith('http')) return;
-			event.respondWith((async (req, evt) => {
-				let requestingClient = await self.clients.get(event.clientId);
+			event.respondWith((async evt => {
+				let requestingClient = await self.clients.get(evt.clientId);
 				this.workport.setCurrentClient(requestingClient);
 				// Now, the following is key:
 				// The browser likes to use "force-cache" for "navigate" requests, when, e.g: re-entering your site with the back button
 				// Problem here, force-cache forces out JSON not HTML as per webflo's design.
 				// So, we detect this scenerio and avoid it.
-				if (req.cache === 'force-cache'/* && req.mode === 'navigate' - even webflo client init call also comes with that... needs investigation */) {
-					req = new Request(req, {cache: 'default'});
+				if (evt.request.cache === 'force-cache'/* && evt.request.mode === 'navigate' - even webflo client init call also comes with that... needs investigation */) {
+					const req = new Request(evt.request, { cache: 'default' });
+					Object.defineProperty(evt, 'request', { value: req });
 				}
-				return this.go(req.url, req, { event: evt });
-			})(event.request, event));
+				return this.go(evt.request.url, evt.request, { event: evt });
+			})(event));
 		});
 
 		// -------------
@@ -134,9 +135,6 @@ export default class Runtime extends _Runtime {
         // ------------
 		// The request object
 		const request = this.generateRequest(url.href, init);
-		if (detail.event) {
-			Object.defineProperty(detail.event, 'request', { value: request });
-		}
 		// The navigation event
 		const httpEvent = new HttpEvent(request, detail, (id = null, persistent = false) => this.getSession(httpEvent, id, persistent));
 		httpEvent.port.listen(message => {
